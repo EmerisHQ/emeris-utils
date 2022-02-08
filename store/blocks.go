fd5ee5171d60fe9d2a93fad42e5382155c92e6b2
Gianguido Sora <gsora@users.noreply.github.com>
2022-02-08 23:45:46 +0100

    feature(store): add LastBlockTime set/get functions (#24)
    Refactor redis querying code to make it common among block funcs.
    Tests for LastBlockTime and Block-related funcs are passing.

--- store/blocks.go
+++ store/blocks.go
@@ -12,6 +12,7 @@ var ErrBlockNotFound = fmt.Errorf("block not found")
 
 const (
 	blockFmt       = "block/%d"
+	blockTimeFmt   = "blockTime/%d"
 	defaultTimeout = 100 * 10 * time.Second // we keep the last 100 blocks, assuming block time of 10 seconds
 )
 
@@ -27,8 +28,12 @@ func blockKey(height int64) string {
 	return fmt.Sprintf(blockFmt, height)
 }
 
-func (b *Blocks) Block(height int64) ([]byte, error) {
-	res, err := b.storeInstance.Client.Get(context.Background(), blockKey(height)).Result()
+func blockTimeKey(height int64) string {
+	return fmt.Sprintf(blockTimeFmt, height)
+}
+
+func (b *Blocks) queryRedis(key string) ([]byte, error) {
+	res, err := b.storeInstance.Client.Get(context.Background(), key).Result()
 	if err != nil {
 		if err == redis.Nil {
 			return nil, ErrBlockNotFound
@@ -38,6 +43,29 @@ func (b *Blocks) Block(height int64) ([]byte, error) {
 	}
 
 	return []byte(res), nil
+}
+
+func (b *Blocks) Block(height int64) ([]byte, error) {
+	return b.queryRedis(blockKey(height))
+}
+
+func (b *Blocks) SetLastBlockTime(t time.Time, height int64) error {
+	// TODO: figure out how to get block time from block
+	mt, err := t.MarshalText()
+	if err != nil {
+		return err
+	}
+	return b.storeInstance.Client.Set(context.Background(), blockTimeKey(height), mt, defaultTimeout).Err()
+}
+
+func (b *Blocks) LastBlockTime(height int64) (time.Time, error) {
+	res, err := b.queryRedis(blockTimeKey(height))
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	tt := time.Time{}
+	return tt, tt.UnmarshalText(res)
 }
 
 func (b *Blocks) Add(data []byte, height int64) error {
