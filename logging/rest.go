d511726d2a29c01d8a3bbbe12b0d39751280c221
Antonio Pitasi <antonio@pitasi.dev>
2022-02-24 11:11:04 +0100

    wip

--- logging/rest.go
+++ logging/rest.go
@@ -1,57 +1,58 @@
 package logging
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
-var l *zap.Logger
+// LogRequest is a gin middleware that logs useful informations on each request
+// as they come.
+//
+// If the request's context contains a logger, it will be used. Otherwise, the
+// specified fallback will be used instead.
+func LogRequest(fallbackLogger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		logger := fallbackLogger
 
-// LogRequest is a gin middleware that logs useful informations on each request as they come.
-func LogRequest(logger *zap.Logger) gin.HandlerFunc {
-	l = logger
-	return log
-}
-func log(c *gin.Context) {
-	ctxLogger, err := GetLoggerFromContext(c)
-	if err != nil && l == nil {
-		panic(fmt.Errorf("Can't get logger from context error:%w", err))
+		// try extract logger from context
+		sugaredLogger := GetLoggerFromContext(c)
+		if sugaredLogger != nil {
+			logger = sugaredLogger.Desugar()
+		}
+
+		// execute the request
+		start := time.Now()
+		c.Next()
+
+		// some evil middlewares modify this values
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+
+		// log request informations
+		logger.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("time", start.Format(time.RFC3339)),
+		)
 	}
-	if ctxLogger != nil {
-		l = ctxLogger.Desugar()
-	}
-	start := time.Now()
-
-	c.Next()
-
-	// some evil middlewares modify this values
-	path := c.Request.URL.Path
-	query := c.Request.URL.RawQuery
-
-	l.Info(path,
-		zap.Int("status", c.Writer.Status()),
-		zap.String("method", c.Request.Method),
-		zap.String("path", path),
-		zap.String("query", query),
-		zap.String("ip", c.ClientIP()),
-		zap.String("user-agent", c.Request.UserAgent()),
-		zap.String("time", start.Format(time.RFC3339)),
-	)
 }
 
-func GetLoggerFromContext(c *gin.Context) (*zap.SugaredLogger, error) {
+func GetLoggerFromContext(c *gin.Context) *zap.SugaredLogger {
 	value, ok := c.Get("logger")
 	if !ok {
-		return nil, fmt.Errorf("logger does not exists in context")
+		return nil
 	}
 
 	l, ok := value.(*zap.SugaredLogger)
 	if !ok {
-		return nil, fmt.Errorf("invalid logger format in context")
+		panic("logger in context is not a *zap.SugaredLogger")
 	}
 
-	return l, nil
+	return l
 }
