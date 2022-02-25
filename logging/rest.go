1cde92d38165ebcc64a97f024b27234f44ad0009
SpideyPool192 <krishna192reddy@gmail.com>
2022-02-25 11:16:43 +0530

    remove global variable

--- logging/rest.go
+++ logging/rest.go
@@ -8,38 +8,37 @@ import (
 	"go.uber.org/zap"
 )
 
-var l *zap.Logger
-
 // LogRequest is a gin middleware that logs useful informations on each request as they come.
-func LogRequest(logger *zap.Logger) gin.HandlerFunc {
-	l = logger
-	return log
-}
-func log(c *gin.Context) {
-	ctxLogger, err := GetLoggerFromContext(c)
-	if err != nil && l == nil {
-		panic(fmt.Errorf("Can't get logger from context error:%w", err))
+func LogRequest(fallBackLogger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		logger := fallBackLogger
+
+		ctxLogger, err := GetLoggerFromContext(c)
+		if err != nil && logger == nil {
+			panic(fmt.Errorf("Can't get logger from context error:%w", err))
+		}
+		if ctxLogger != nil {
+			logger = ctxLogger.Desugar()
+		}
+
+		start := time.Now()
+
+		c.Next()
+
+		// some evil middlewares modify this values
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+
+		logger.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("time", start.Format(time.RFC3339)),
+		)
 	}
-	if ctxLogger != nil {
-		l = ctxLogger.Desugar()
-	}
-	start := time.Now()
-
-	c.Next()
-
-	// some evil middlewares modify this values
-	path := c.Request.URL.Path
-	query := c.Request.URL.RawQuery
-
-	l.Info(path,
-		zap.Int("status", c.Writer.Status()),
-		zap.String("method", c.Request.Method),
-		zap.String("path", path),
-		zap.String("query", query),
-		zap.String("ip", c.ClientIP()),
-		zap.String("user-agent", c.Request.UserAgent()),
-		zap.String("time", start.Format(time.RFC3339)),
-	)
 }
 
 func GetLoggerFromContext(c *gin.Context) (*zap.SugaredLogger, error) {
