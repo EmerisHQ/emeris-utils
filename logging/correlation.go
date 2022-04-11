b0c2842821f463fa228e96636183658fcc970416
Antonio Pitasi <antonio@pitasi.dev>
2022-04-11 09:44:57 +0200

    feat: add request params to gin logger

--- logging/correlation.go
+++ logging/correlation.go
@@ -16,14 +16,16 @@ const (
 	ExternalCorrelationIDName string = "X-Correlation-Id"
 )
 
-// CorrelationIDMiddleware adds correlationID if it's not specified in HTTP request
-func CorrelationIDMiddleware(l *zap.SugaredLogger) gin.HandlerFunc {
+// AddLoggerMiddleware adds a logger to the gin context, with some fields
+// populated (correlation ID, requests params, ...).
+// The logger can be retrieved by calling GetLoggerFromContext(c).
+func AddLoggerMiddleware(l *zap.SugaredLogger) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		addCorrelationID(c, l)
+		addLogger(c, l)
 	}
 }
 
-func addCorrelationID(c *gin.Context, l *zap.SugaredLogger) {
+func addLogger(c *gin.Context, l *zap.SugaredLogger) {
 	ctx := c.Request.Context()
 
 	correlationID := c.Request.Header.Get(ExternalCorrelationIDName)
@@ -41,6 +43,14 @@ func addCorrelationID(c *gin.Context, l *zap.SugaredLogger) {
 
 	ctx = context.WithValue(ctx, IntCorrelationIDName, id.String())
 	l = l.With(string(IntCorrelationIDName), id)
+
+	for _, p := range c.Params {
+		l = l.With(p.Key, p.Value)
+	}
+
+	if len(c.Request.URL.RawQuery) > 0 {
+		l = l.With("query", c.Request.URL.RawQuery)
+	}
 
 	c.Set("logger", l)
 
