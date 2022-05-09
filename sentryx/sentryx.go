653561f83603e856959b5076290fe914c75d71aa
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2022-05-09 15:23:36 +0200

    fix/sentry: middlewares and span creation method
    Since discussions we had, I suggest the following change:
    
    - Merge ginsentry into sentryx (because of possible confusion with
      the legacy sentrygin package)
    - Rename Middleware to GinMiddleware (because of the package change)
    - Remove Recover since now it's embeded in the middleware
    - Catch panic in GinMiddleware, send it to Sentry and repanic.
    - Prefix URL params with `params`
    - StartSpan ensures the context used is not `gin.Context`.
    - Prefix params transaction tags with `param.`

--- sentryx/sentryx.go
+++ sentryx/sentryx.go
@@ -2,9 +2,45 @@ package sentryx
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/getsentry/sentry-go"
+	"github.com/gin-gonic/gin"
 )
+
+// GinMiddleware returns a middlewares that properly logs the query and its
+// error in Sentry.
+func GinMiddleware(c *gin.Context) {
+	ctx := c.Request.Context()
+	hub := sentry.GetHubFromContext(ctx)
+	if hub == nil {
+		hub = sentry.CurrentHub().Clone()
+		ctx = sentry.SetHubOnContext(ctx, hub)
+	}
+	c.Request = c.Request.WithContext(ctx)
+	hub.Scope().SetRequest(c.Request)
+
+	// start span for the request
+	span, ctx := StartSpan(c.Request.Context(), "http.server",
+		sentry.TransactionName(fmt.Sprintf("%s %s", c.Request.Method, c.FullPath())),
+		sentry.ContinueFromRequest(c.Request),
+	)
+	for _, param := range c.Params {
+		span.SetTag("param."+param.Key, param.Value)
+	}
+	defer func() {
+		span.Finish()
+		if err := recover(); err != nil {
+			// Send error to sentry
+			hub.RecoverWithContext(ctx, err)
+			// repanic
+			panic(err)
+		}
+	}()
+	c.Request = c.Request.WithContext(ctx)
+
+	c.Next()
+}
 
 // StartSpan calls sentry.StartSpan and returns the span and the span context.
 // This is handy to ensure correct hierarchy of span if other spans are started
@@ -14,6 +50,11 @@ import (
 //     span, ctx := sentryx.StartSpan(ctx, "operation")
 //     defer span.Finish()
 func StartSpan(ctx context.Context, operation string, options ...sentry.SpanOption) (*sentry.Span, context.Context) {
+	if ginctx, ok := ctx.(*gin.Context); ok {
+		// Ensure we use the good context (released version of gin doesn't fallback
+		// to the request context).
+		ctx = ginctx.Request.Context()
+	}
 	s := sentry.StartSpan(ctx, operation, options...)
 	return s, s.Context()
 }
